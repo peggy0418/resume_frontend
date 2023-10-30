814cc77d2b7e200013a8ee82b23e457679528576
Peggy <but5863841@gmail.com>
2023-10-30 11:30:36 +0800

    feat: :zap: 增加註冊登入的路由

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/message">留言板</router-link>
+    <router-link to="/message">留言板</router-link> |
+    <router-link to="/register">註冊</router-link> |
+    <router-link to="/login">登入</router-link> 
     
   </nav>
   <router-view/>
