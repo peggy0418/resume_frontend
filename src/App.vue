4adcbfc153128b6ddadcf36bc1da091fda050748
Peggy <but5863841@gmail.com>
2023-11-03 11:19:55 +0800

    feat: :zap: 加上tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,125 @@
 <template>
-  <Disclosure as="nav" class="bg-black shadow" v-slot="{ open }">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div class="flex h-16 justify-between">
-        <div class="flex">
-          <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-          </div>
-          <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
-            <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-            <router-link class="inline-flex items-center border-b-2 border-blue px-1 pt-1 text-sm font-medium text-white" to="/">登入</router-link>
-            <router-link class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-blue hover:text-lg hover:font-bold" to="/resume">履歷表</router-link>
-            <router-link class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-blue hover:text-lg hover:font-bold" to="message">留言板</router-link>
-            <router-link class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-blue hover:text-lg hover:font-bold" to="register">註冊</router-link>
-          </div>
+  <Disclosure as="nav" class="bg-white shadow" v-slot="{ open }">
+    <div class="sticky flex h-16 justify-between bg-black w-full ">
+      <div class="flex">
+        <div class="flex flex-shrink-0 items-center">
+          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company" />
         </div>
-        <div class="hidden sm:ml-6 sm:flex sm:items-center">
-          <button type="button" class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            <span class="absolute -inset-1.5" />
-            <span class="sr-only">View notifications</span>
-            <BellIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
-
-          <!-- Profile dropdown -->
-          <Menu as="div" class="relative ml-3">
-            <div>
-              <MenuButton class="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                <span class="absolute -inset-1.5" />
-                <span class="sr-only">Open user menu</span>
-                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              </MenuButton>
-            </div>
-            <transition enter-active-class="transition ease-out duration-200" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-              <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <MenuItem v-slot="{ active }">
-                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
-                </MenuItem>
-              </MenuItems>
-            </transition>
-          </Menu>
-        </div>
-        <div class="-mr-2 flex items-center sm:hidden">
-          <!-- Mobile menu button -->
-          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-            <span class="absolute -inset-0.5" />
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </DisclosureButton>
+        <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
+          <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
+          <router-link class="inline-flex items-center border-b-4 border-blue px-1 pt-1 text-sm font-medium text-white"
+            to="/">登入</router-link>
+          <router-link
+            class="inline-flex items-center border-b-4 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-blue hover:text-lg hover:font-bold"
+            to="/resume">履歷表</router-link>
+          <router-link
+            class="inline-flex items-center border-b-4 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-blue hover:text-lg hover:font-bold"
+            to="message">留言板</router-link>
+          <router-link
+            class="inline-flex items-center border-b-4 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-blue hover:text-lg hover:font-bold"
+            to="register">註冊</router-link>
         </div>
       </div>
-      <router-view/>
+      <div class="hidden sm:ml-6 sm:flex sm:items-center">
+        <button type="button"
+          class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          <span class="absolute -inset-1.5" />
+          <span class="sr-only">View notifications</span>
+          <BellIcon class="h-6 w-6" aria-hidden="true" />
+        </button>
+        <!-- Profile dropdown -->
+        <Menu as="div" class="relative ml-3">
+          <div>
+            <MenuButton
+              class="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <span class="absolute -inset-1.5" />
+              <span class="sr-only">Open user menu</span>
+              <img class="h-8 w-8 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt="" />
+            </MenuButton>
+          </div>
+          <transition enter-active-class="transition ease-out duration-200"
+            enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95">
+            <MenuItems
+              class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <MenuItem v-slot="{ active }">
+              <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+              <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+              <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
+              </MenuItem>
+            </MenuItems>
+          </transition>
+        </Menu>
+      </div>
+      <div class="-mr-2 flex items-center sm:hidden">
+        <!-- Mobile menu button -->
+        <DisclosureButton
+          class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <span class="absolute -inset-0.5" />
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+          <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+        </DisclosureButton>
+      </div>
     </div>
 
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 pb-3 pt-2">
         <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
-        <DisclosureButton as="a" href="#" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">Dashboard</DisclosureButton>
-        <DisclosureButton as="a" href="#" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Team</DisclosureButton>
-        <DisclosureButton as="a" href="#" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Projects</DisclosureButton>
-        <DisclosureButton as="a" href="#" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Calendar</DisclosureButton>
+        <DisclosureButton as="a" href="#"
+          class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">
+          Dashboard</DisclosureButton>
+        <DisclosureButton as="a" href="#"
+          class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">
+          Team</DisclosureButton>
+        <DisclosureButton as="a" href="#"
+          class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">
+          Projects</DisclosureButton>
+        <DisclosureButton as="a" href="#"
+          class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">
+          Calendar</DisclosureButton>
       </div>
       <div class="border-t border-gray-200 pb-3 pt-4">
         <div class="flex items-center px-4">
           <div class="flex-shrink-0">
-            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+            <img class="h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt="" />
           </div>
           <div class="ml-3">
             <div class="text-base font-medium text-gray-800">Tom Cook</div>
             <div class="text-sm font-medium text-gray-500">tom@example.com</div>
           </div>
-          <button type="button" class="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          <button type="button"
+            class="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             <span class="absolute -inset-1.5" />
             <span class="sr-only">View notifications</span>
             <BellIcon class="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
         <div class="mt-3 space-y-1">
-          <DisclosureButton as="a" href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">Your Profile</DisclosureButton>
-          <DisclosureButton as="a" href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">Settings</DisclosureButton>
-          <DisclosureButton as="a" href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">Sign out</DisclosureButton>
+          <DisclosureButton as="a" href="#"
+            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">Your
+            Profile</DisclosureButton>
+          <DisclosureButton as="a" href="#"
+            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">Settings
+          </DisclosureButton>
+          <DisclosureButton as="a" href="#"
+            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">Sign out
+          </DisclosureButton>
         </div>
       </div>
     </DisclosurePanel>
   </Disclosure>
+  <router-view />
 </template>
 
 <script setup>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,96 +1,66 @@
+<script lang="ts" setup>
+import axios from "axios";
+import { ref } from 'vue';
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const user_email = ref('');
+const user_password = ref('');
+
+const loginUser = () => {
+  // 在这里可以执行登录逻辑，发送用户输入到后端进行验证
+  // 你可以使用 Axios 或 Fetch API 来发送登录请求
+
+  // 构建一个包含用户登录信息的对象
+  const userData = {
+    user_email: user_email.value,
+    user_password: user_password.value,
+  };
+
+  // 发送用户数据到后端登录接口
+  axios.post('/api/login', userData)
+    .then(response => {
+      if (response.data.error) {
+        // 处理登录错误
+        alert('登入失敗：' + response.data.error);
+      } else {
+        // 处理成功登录的情况
+        alert('登入成功');
+
+        // 你可以执行页面跳转或其他操作
+        // 例如，使用 Vue Router 来跳转到用户的个人资料页
+        router.push({ name: 'message' }); // 假设你有一个名为 'profile' 的路由
+      }
+    })
+    .catch(error => {
+      console.error('網路異常：', error);
+    });
+};
+</script>
+
 <template>
-    <div class="login">
-      <h2>使用者登入</h2>
+  <div class="login flex items-center justify-center bg-gray-100 m-48">
+    <div class="max-w-md w-full p-4 bg-white rounded-lg shadow-md border-2">
+      <h2 class="text-2xl font-semibold text-center mb-4">使用者登入</h2>
       <form @submit.prevent="loginUser">
-        <div class="form-group">
-          <label for="user_email">電子郵件</label>
-          <input type="text" id="user_email" v-model="user_email" required>
+        <div class="mb-4">
+          <label for="user_email" class="block text-sm font-medium text-gray-600">電子郵件</label>
+          <input type="text" id="user_email" v-model="user_email" required
+                 class="w-full px-4 py-2 rounded-md border focus:ring-indigo-500 focus:border-indigo-500
+                 shadow-sm focus:ring-2 focus:ring-offset-2">
         </div>
-        <div class="form-group">
-          <label for="user_password">密碼</label>
-          <input type="password" id="user_password" v-model="user_password" required>
+        <div class="mb-4">
+          <label for="user_password" class="block text-sm font-medium text-gray-600">密碼</label>
+          <input type="password" id="user_password" v-model="user_password" required
+                 class="w-full px-4 py-2 rounded-md border focus:ring-indigo-500 focus:border-indigo-500
+                 shadow-sm focus:ring-2 focus:ring-offset-2">
         </div>
-        <button type="submit">登入</button>
+        <button type="submit" class="w-full px-4 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-700
+            focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">登入</button>
       </form>
     </div>
-  </template>
-  
-  <script>
-  import axios from "axios"
-  export default {
-    data() {
-      return {
-        user_email: '',
-        user_password: ''
-      };
-    },
-    methods: {
-      loginUser() {
-        // 在这里可以执行登录逻辑，发送用户输入到后端进行验证
-        // 你可以使用 Axios 或 Fetch API 来发送登录请求
-  
-        // 构建一个包含用户登录信息的对象
-        const userData = {
-          user_email: this.user_email,
-          user_password: this.user_password
-        };
-  
-        // 发送用户数据到后端登录接口
-        axios.post('/api/login', userData)
-          .then(response => {
-            if (response.data.error) {
-              // 处理登录错误
-              alert('登录失败：' + response.data.error);
-            } else {
-              // 处理成功登录的情况
-              alert('登录成功：' + response.data.message);
-  
-              // 你可以执行页面跳转或其他操作
-              // 例如，使用Vue Router来跳转到用户的个人资料页
-              this.$router.push({ name: 'message' }); // 假设你有一个名为'profile'的路由
-            }
-          })
-          .catch(error => {
-            console.error('发生网络错误：', error);
-          });
-      }
-    }
-  };
-  </script>
-  
-  
-  <style scoped>
-  .login {
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-  
-  .form-group {
-    margin-bottom: 15px;
-  }
-  
-  label {
-    display: block;
-    font-weight: bold;
-  }
-  
-  input[type="text"],
-  input[type="password"] {
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 10px;
-  }
-  
-  button {
-    background-color: #007bff;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  </style>
-  
+  </div>
+</template>
+
+
