2d0713e762c48b825958f403fdc2f70a235da5f4
peggy0418 <but5863841@gmail.com>
2023-11-07 11:26:13 +0800

    fix: :bug: 修改樣式

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         </div>
         <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
           <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-          <router-link class="inline-flex items-center border-b-4 border-blue px-1 pt-1 text-sm font-medium text-white"
+          <router-link class="inline-flex items-center border-b-4 hover:border-blue px-1 pt-1 text-sm font-medium text-white"
             to="/">登入</router-link>
           <router-link
             class="inline-flex items-center border-b-4 border-transparent px-1 pt-1 text-sm font-medium text-white hover:border-blue hover:text-lg hover:font-bold"
